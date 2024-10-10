af8dbe3a8a461f1939ebc86c2effbd4098be58bd
Md Khayrul Hasan <khayrul.web@gmail.com>
2024-10-10 11:33:47 +0600

    Project Initialize

---
+++ src/main/java/com/binarydesign/techledger/TechledgerApplication.java
@@ -0,0 +1,13 @@
+package com.binarydesign.techledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechledgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechledgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binarydesign/techledger/TechledgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binarydesign.techledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechledgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
